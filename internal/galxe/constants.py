1edc881ee681b76a050aa761a14d1a20a01de177
the-laziest-coder <timagrib1111@gmail.com>
2024-04-21 21:29:20 +0200

    Matic to Polygon chain mapping

--- internal/galxe/constants.py
+++ internal/galxe/constants.py
@@ -1,6 +1,6 @@
 DISCORD_AUTH_URL = 'https://discord.com/api/v9/oauth2/authorize'
 GALXE_DISCORD_CLIENT_ID = '947863296789323776'
 
-CHAIN_NAME_MAPPING = {}
+CHAIN_NAME_MAPPING = {'MATIC': 'Polygon'}
 
 VERIFY_TRIES = 4
